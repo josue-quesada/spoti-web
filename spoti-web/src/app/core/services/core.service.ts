f29f5e1ccd426841fe5b877d64129c3e455c9541
Josué Quesada <51096767+josue-quesada@users.noreply.github.com>
2024-01-18 23:00:21 -0600

    update

--- spoti-web/src/app/core/services/core.service.ts
+++ spoti-web/src/app/core/services/core.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, of } from 'rxjs';
 import { SpotiToken } from '../interfaces/spotify.interface';
+import { Artist } from '../interfaces/artist.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class CoreService {
   private clientId: string = "b0ccf348577d4feaab3c786a86f3248b";
   private clientSecret: string = "39ea6a9ef1f14f0790201053ada1184f";
   tokenUrl: string = "https://accounts.spotify.com/api/token";
-  url: string = "";
+  private url: string = "https://api.spotify.com/v1/";
   idAndSecret: string = btoa(this.clientId + ":" + this.clientSecret);
-  private token: string = "https://api.spotify.com/v1/";
+  private token: string = "";
 
   body = "grant_type=client_credentials";
 
@@ -23,11 +24,11 @@ export class CoreService {
     }),
   };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAccessToken_(): string {
+  getAccessToken(): string {
     const url = this.tokenUrl;
-    this.httpClient
+    this.http
       .post<SpotiToken>(url, this.body, this.options)
       .pipe(
         map((response) => response.access_token),
@@ -40,8 +41,11 @@ export class CoreService {
     return this.token;
   }
 
-  getTracks(name: string) {
-
+  getArtistId(name: string) {
+    const url = `${this.url}?q=${name}&type=artist`
+    this.http.get<Artist[]>(url).subscribe(resp => {
+      console.log(resp)
+    });
   }
 
 }
